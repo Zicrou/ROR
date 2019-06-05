029dc5274b26f7d6e6ff9779ce7d2f0a91feed42
Zicrou <aazseck@gmail.com>
2019-06-05 19:11:23 +0000

    End_Last Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-http_basic_authenticate_with name: "zicrou", password: "ZicroU", only: :destroy
+http_basic_authenticate_with name: "zicrou", password: "Zicrou", only: :destroy
 
     def create
       @word = Word.find(params[:word_id])
